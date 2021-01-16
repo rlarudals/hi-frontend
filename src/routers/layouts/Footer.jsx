ea202f8bee140e96046bc6f9254be41e9b3049c4
YERIM <dnaer456@gmail.com>
2021-01-16 17:58:51 +0900

    Footer Wrapper

--- src/routers/layouts/Footer.jsx
+++ src/routers/layouts/Footer.jsx
@@ -79,20 +79,19 @@ const Footer = () => {
             <img src={logoimg} width="190px" height="30px" />
           </LogoBox>
         </FooterWrapper>
-        <FooterWrapper>
-          <FooterBox>
-            <TextBox width={`100%`} height={`300px`}>
-              <Text>(주)단순한주식회사</Text>
-              <Text>사업자 등록번호 : 14523-94-03562</Text>
-              <Text>통신판매업신고번호 : 제 2020-충남공주-1002</Text>
-              <Text>법인명 : (주)health</Text>
-              <Text>대표이사 : 김경민,방효진,정예림</Text>
-              <Text>개인정보관리책임자 : 김경민,방효진,정예림</Text>
-              <Text>주소 : 충남 공주시 우금티로 530</Text>
-              <Text>© 2021 health</Text>
-            </TextBox>
-          </FooterBox>
-        </FooterWrapper>
+
+        <FooterBox>
+          <TextBox width={`100%`} height={`300px`}>
+            <Text>(주)단순한주식회사</Text>
+            <Text>사업자 등록번호 : 14523-94-03562</Text>
+            <Text>통신판매업신고번호 : 제 2020-충남공주-1002</Text>
+            <Text>법인명 : (주)health</Text>
+            <Text>대표이사 : 김경민,방효진,정예림</Text>
+            <Text>개인정보관리책임자 : 김경민,방효진,정예림</Text>
+            <Text>주소 : 충남 공주시 우금티로 530</Text>
+            <Text>© 2021 health</Text>
+          </TextBox>
+        </FooterBox>
       </FooterAreaContent>
     </FooterArea>
   );
