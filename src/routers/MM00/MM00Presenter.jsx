244fee74c99031644ad5bbc11dfe0f5d876c69fc
rlarudals <gim181004@gmail.com>
2021-01-18 19:58:25 +0900

    slide

--- src/routers/MM00/MM00Presenter.jsx
+++ src/routers/MM00/MM00Presenter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Wrapper, ImageBox } from "../../components/commonComponent";
 import styled from "styled-components";
 import Fade from "react-reveal/Fade";
+import Carousel from "react-elastic-carousel";
 
 const ViewText = styled.div`
   margin: 6px;
@@ -15,6 +16,10 @@ const ViewText = styled.div`
 const WrapWrapper = styled(Wrapper)`
   width: 1350px;
   flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 애
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
 `;
 
 const FileInput = styled.input`
@@ -34,28 +39,31 @@ const FileLabel = styled.label`
   cursor: pointer;
 `;
 
-const Image = styled.img`
-  width: 400px;
-  height: 400px;
-  border: 2px solid #777;
-  margin: 10px;
-  object-fit: cover;
-`;
-
 const MM00Presenter = ({ fileChangeHandler, imagePath }) => {
   return (
     <Wrapper>
       <Wrapper hidden={`400px`} padding={`25px`} dr={`row`}>
         <Fade left>
           <Wrapper al={`flex-end`} padding={`20px`}>
-            <ImageBox
-              width={`400px`}
-              height={`225px`}
-              bgImg={`https://images.unsplash.com/photo-1538108149393-fbbd81895907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2900&q=80`}
-            />
+            <Carousel enableAutoPlay autoPlaySpeed={1500}>
+              <ImageBox
+                width={`400px`}
+                height={`200px`}
+                bgImg={`https://images.unsplash.com/photo-1538108149393-fbbd81895907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2900&q=80`}
+              />
+              <ImageBox
+                width={`400px`}
+                height={`200px`}
+                bgImg={`https://images.unsplash.com/photo-1551076805-e1869033e561?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2390&q=80`}
+              />
+              <ImageBox
+                width={`400px`}
+                height={`200px`}
+                bgImg={`https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1635&q=80`}
+              />
+            </Carousel>
           </Wrapper>
         </Fade>
-
         <Fade right>
           <Wrapper al={`flex-start`} padding={`20px`}>
             <ViewText size={`20px`} weight={`700`} isTitle={true}>
