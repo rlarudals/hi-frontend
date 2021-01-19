7cfe761ae0ac68a23bbd21e9f4836b99106c88df
YERIM <dnaer456@gmail.com>
2021-01-20 02:06:50 +0900

    MM02 styled start

--- src/routers/MM02/MM02Presenter.jsx
+++ src/routers/MM02/MM02Presenter.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
+
+const MM02Area = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+`;
+
+const Box = styled.div`
+  width: 200px;
+`;
 
 const MM02Presenter = () => {
-  return `Hello MM02`;
+  return <MM02Area>정신건강</MM02Area>;
 };
 
 export default MM02Presenter;
