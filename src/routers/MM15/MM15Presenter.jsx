be9fd0d51ac5f0a4b803ba4f3ec39829fb914f68
HJ <user@users-MacBook-Pro.local>
2021-01-19 23:35:15 +0900

    MM15 image

--- src/routers/MM15/MM15Presenter.jsx
+++ src/routers/MM15/MM15Presenter.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-import { Wrapper, ImageBox } from "../../components/commonComponent";
+import { Wrapper } from "../../components/commonComponent";
 import eatinglifea from "../../img/eatinglifea.png";
 import eatinglifeb from "../../img/eatinglifeb.png";
+import eatinglifec from "../../img/eatinglifec.png";
+import eatinglifed from "../../img/eatinglifed.png";
+import eatinglifee from "../../img/eatinglifee.png";
+import eatinglifef from "../../img/eatinglifef.png";
+import eatinglifeg from "../../img/eatinglifeg.png";
+import eatinglifeh from "../../img/eatinglifeh.png";
 
 const InnerWrapper = styled.div`
   width: ${(props) => props.width || `100%`};
@@ -12,17 +18,12 @@ const InnerWrapper = styled.div`
   flex-direction: row;
   align-items: center;
   justify-content: center;
+  flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 애
 `;
 
 const ImageWrapper = styled.div`
   height: 200px;
   margin: 20px;
-  transition: 0.3s;
-
-  &:hover {
-    box-shadow: ${(props) => props.theme.boxShadow};
-    opacity: 0.8;
-  }
 `;
 
 const SpanText = styled.span`
@@ -34,6 +35,19 @@ const SpanText = styled.span`
   text-shadow: 2px 2px 0px #a7e074, 5px 5px 0px #c0ec83, 8px 8px 0px #e3f2fd;
 `;
 
+const ImageBox = styled.img`
+  width: 300px;
+  height: 200px;
+  border-radius: 10px;
+
+  transition: 0.3s;
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.boxShadow};
+    opacity: 0.8;
+  }
+`;
+
 const MM15Presenter = () => {
   return (
     <Wrapper>
@@ -41,12 +55,42 @@ const MM15Presenter = () => {
       <InnerWrapper>
         <ImageWrapper>
           <a href="https://www.youtube.com/watch?v=WBtbMLk43a0">
-            <img src={eatinglifea} width="300px" height="200px" />
+            <ImageBox src={eatinglifea} width="300px" height="200px" />
           </a>
         </ImageWrapper>
         <ImageWrapper>
           <a href="https://www.youtube.com/watch?v=io6RQ-4JrtM">
-            <img src={eatinglifeb} width="300px" height="200px" />
+            <ImageBox src={eatinglifeb} width="300px" height="200px" />
+          </a>
+        </ImageWrapper>
+        <ImageWrapper>
+          <a href="https://www.youtube.com/watch?v=NUN7Sk-OUdM">
+            <ImageBox src={eatinglifec} width="300px" height="200px" />
+          </a>
+        </ImageWrapper>
+        <ImageWrapper>
+          <a href="https://www.youtube.com/watch?v=GFJzE0Kk4Rk">
+            <ImageBox src={eatinglifed} width="300px" height="200px" />
+          </a>
+        </ImageWrapper>
+        <ImageWrapper>
+          <a href="https://www.youtube.com/watch?v=3goYXaT24F0">
+            <ImageBox src={eatinglifee} width="300px" height="200px" />
+          </a>
+        </ImageWrapper>
+        <ImageWrapper>
+          <a href="https://www.youtube.com/watch?v=B5xDw6nhY5U">
+            <ImageBox src={eatinglifef} width="300px" height="200px" />
+          </a>
+        </ImageWrapper>
+        <ImageWrapper>
+          <a href="https://www.youtube.com/watch?v=qqel92gLPjg">
+            <ImageBox src={eatinglifeg} width="300px" height="200px" />
+          </a>
+        </ImageWrapper>{" "}
+        <ImageWrapper>
+          <a href="https://www.youtube.com/watch?v=2cMl_kW4Zi8&t=5s">
+            <ImageBox src={eatinglifeh} width="300px" height="200px" />
           </a>
         </ImageWrapper>
       </InnerWrapper>
