422724a9a0333ca8c72e703c3d0f5cf71fe30d82
rlarudals <gim181004@gmail.com>
2021-01-19 21:44:28 +0900

    slide update

--- src/routers/MM00/MM00Presenter.jsx
+++ src/routers/MM00/MM00Presenter.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-import { Wrapper, ImageBox } from "../../components/commonComponent";
+import { Wrapper } from "../../components/commonComponent";
 import styled from "styled-components";
 import Fade from "react-reveal/Fade";
-import Carousel from "react-elastic-carousel";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 const ViewText = styled.div`
   margin: 5px;
@@ -26,12 +28,43 @@ const FileLabel = styled.label`
   cursor: pointer;
 `;
 
+const settings = {
+  dots: false,
+  infinite: true,
+  speed: 2500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  pauseOnHover: false,
+  autoplaySpeed: 4000,
+  arrows: true,
+};
+
 const MM00Presenter = () => {
   return (
     <Wrapper>
       <Wrapper hidden={`400px`} padding={`25px`} dr={`row`}>
         <Fade left>
-          <ImageBox></ImageBox>
+          <Slider {...settings}>
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+            <div>
+              <h3>6</h3>
+            </div>
+          </Slider>
         </Fade>
         <Fade right>
           <Wrapper>
