010c549b36076ecb4dd271b2356016c86bebc551
rlarudals <gim181004@gmail.com>
2021-01-19 20:58:00 +0900

    MM00

--- src/routers/MM00/MM00Presenter.jsx
+++ src/routers/MM00/MM00Presenter.jsx
@@ -5,25 +5,12 @@ import Fade from "react-reveal/Fade";
 import Carousel from "react-elastic-carousel";
 
 const ViewText = styled.div`
-  margin: 6px;
+  margin: 5px;
   font-size: ${(props) => props.size};
-  font-weight: ${(props) => props.weight};
-  color: ${(props) => (props.isTitle ? props.theme.blackColor : ``)};
-  color: ${(props) => (props.isAuthor ? props.theme.whiteColor : ``)};
-  color: ${(props) => (props.isDesc ? props.theme.greyColor : ``)};
-`;
-
-const WrapWrapper = styled(Wrapper)`
-  width: 1350px;
-  flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 애
   display: flex;
   flex-direction: row;
-  align-items: center;
-  justify-content: center;
-`;
-
-const FileInput = styled.input`
-  display: none;
+  align-items: flex-end;
+  justify-content: flex-end;
 `;
 
 const FileLabel = styled.label`
@@ -39,40 +26,22 @@ const FileLabel = styled.label`
   cursor: pointer;
 `;
 
-const MM00Presenter = ({ fileChangeHandler, imagePath }) => {
+const MM00Presenter = () => {
   return (
     <Wrapper>
       <Wrapper hidden={`400px`} padding={`25px`} dr={`row`}>
         <Fade left>
-          <Wrapper al={`flex-end`} padding={`20px`}>
-            <Carousel enableAutoPlay autoPlaySpeed={1500}>
-              <ImageBox
-                width={`400px`}
-                height={`200px`}
-                bgImg={`https://images.unsplash.com/photo-1538108149393-fbbd81895907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2900&q=80`}
-              />
-              <ImageBox
-                width={`400px`}
-                height={`200px`}
-                bgImg={`https://images.unsplash.com/photo-1551076805-e1869033e561?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=2390&q=80`}
-              />
-              <ImageBox
-                width={`400px`}
-                height={`200px`}
-                bgImg={`https://images.unsplash.com/photo-1519494026892-80bbd2d6fd0d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1635&q=80`}
-              />
-            </Carousel>
-          </Wrapper>
+          <ImageBox></ImageBox>
         </Fade>
         <Fade right>
-          <Wrapper al={`flex-start`} padding={`20px`}>
-            <ViewText size={`20px`} weight={`700`} isTitle={true}>
+          <Wrapper>
+            <ViewText size={`20px`} isTitle={true}>
               Text 1
             </ViewText>
-            <ViewText size={`15px`} weight={`400`} isTitle={true}>
+            <ViewText size={`15px`} isTitle={true}>
               Text 2
             </ViewText>
-            <ViewText size={`13px`} weight={`500`} isTitle={true}>
+            <ViewText size={`13px`} isTitle={true}>
               Text 3
             </ViewText>
           </Wrapper>
