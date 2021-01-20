be1ca9eac5a3afa44b164e262401e351f355f342
YERIM <dnaer456@gmail.com>
2021-01-21 01:14:49 +0900

    MM02 in video & MM06 png

--- src/routers/MM02/MM02Presenter.jsx
+++ src/routers/MM02/MM02Presenter.jsx
@@ -1,19 +1,133 @@
 import React from "react";
 import styled from "styled-components";
+import mentalhealth from "../../img/mentalhealth.png";
+import mentalhealtha from "../../img/mentalhealtha.png";
+import mentalhealthb from "../../img/mentalhealthb.png";
+import mentalhealthc from "../../img/mentalhealthc.png";
 
 const MM02Area = styled.div`
   width: 100%;
 
   display: flex;
+  flex-direction: column;
+`;
+
+const TitleBox = styled.div`
+  width: 300px;
+  height: 50px;
+
+  margin-left: 30px;
+  margin-bottom: 35px;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+
+  &:hover {
+    font-weight: 600;
+  }
+`;
+
+const Title = styled.span`
+  font-size: 25px;
+
+  display: flex;
   flex-direction: row;
 `;
 
-const Box = styled.div`
+const InnerWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 애
+`;
+
+const DetailWrapper = styled.div`
+  width: 250px;
+  height: 170px;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 10px;
+  margin-left: 80px;
+  margin-bottom: 50px;
+`;
+
+const DetailTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  margin-bottom: 50px;
+  margin-top: 15px;
+`;
+
+const ImageBox = styled.img`
   width: 200px;
+  height: 120px;
+
+  border-radius: 10px;
+
+  transition: 0.5s;
+  margin-left: 40px;
+
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.boxShadow};
+    opacity: 0.8;
+  }
 `;
 
 const MM02Presenter = () => {
-  return <MM02Area>정신건강</MM02Area>;
+  return (
+    <MM02Area>
+      <TitleBox>
+        <Title>학생 정신건강 동영상 🖥</Title>
+      </TitleBox>
+      <InnerWrapper>
+        <DetailWrapper>
+          <a href="https://www.youtube.com/watch?v=ISzkvqwT5ho">
+            <ImageBox src={mentalhealth} />
+          </a>
+          <DetailTitle>학생정서교육-우울</DetailTitle>
+        </DetailWrapper>
+
+        <DetailWrapper>
+          <a href="https://www.youtube.com/watch?v=AAXH9c9NQPA">
+            <ImageBox src={mentalhealtha} />
+          </a>
+          <DetailTitle>학생정서교육-분노조절</DetailTitle>
+        </DetailWrapper>
+
+        <DetailWrapper>
+          <a href="https://www.youtube.com/watch?v=BxJfN_0kZK8">
+            <ImageBox src={mentalhealthb} />
+          </a>
+          <DetailTitle>정신력 강화를 위한 9가지 두뇌 운동</DetailTitle>
+        </DetailWrapper>
+
+        <DetailWrapper>
+          <a href="https://www.youtube.com/watch?v=gLqg-wU_WQc">
+            <ImageBox src={mentalhealthc} />
+          </a>
+          <DetailTitle>스트레스를 무시하면 안되는 6가지 이유</DetailTitle>
+        </DetailWrapper>
+      </InnerWrapper>
+    </MM02Area>
+  );
 };
 
 export default MM02Presenter;
--- src/routers/MM06/MM06Presenter.jsx
+++ src/routers/MM06/MM06Presenter.jsx
@@ -1,7 +1,88 @@
 import React from "react";
+import styled from "styled-components";
+import mentalhealth from "../../img/mentalhealth.png";
+
+const InnerWrapper = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
+  flex-wrap: wrap; // width가 꽉 차면 자동으로 내려주는 애
+`;
+
+const DetailWrapper = styled.div`
+  width: 250px;
+  height: 170px;
+
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  margin-top: 10px;
+  margin-left: 50px;
+  margin-bottom: 50px;
+`;
+
+const DetailTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  align-items: center;
+  justify-content: center;
+
+  margin-bottom: 50px;
+  margin-top: 15px;
+`;
+
+const ImageBox = styled.img`
+  width: 200px;
+  height: 120px;
+
+  border-radius: 10px;
+
+  transition: 0.5s;
+  margin-left: 40px;
+
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.boxShadow};
+    opacity: 0.8;
+  }
+`;
 
 const MM06Presenter = () => {
-  return `Hello MM06`;
+  return (
+    <InnerWrapper>
+      <DetailWrapper>
+        <ImageBox src={mentalhealth} />
+        <a href=""></a>
+        <DetailTitle>학생자살예방교육-고등용</DetailTitle>
+      </DetailWrapper>
+
+      <DetailWrapper>
+        <ImageBox src={mentalhealth} />
+        <a href="https://www.youtube.com/watch?v=e_XMM98oV80t=316s"></a>
+        <DetailTitle>학생자살예방교육-중등용</DetailTitle>
+      </DetailWrapper>
+
+      <DetailWrapper>
+        <ImageBox src={mentalhealth} />
+        <a href="https://www.youtube.com/watch?v=dkcfXFx2U2I"></a>
+        <DetailTitle>학생자살예방교육-중등용</DetailTitle>
+      </DetailWrapper>
+
+      <DetailWrapper>
+        <ImageBox src={mentalhealth} />
+        <a href="https://www.youtube.com/watch?v=0n7lJG2PSKQ"></a>
+        <DetailTitle>생명존중과 자살예방</DetailTitle>
+      </DetailWrapper>
+    </InnerWrapper>
+  );
 };
 
 export default MM06Presenter;
