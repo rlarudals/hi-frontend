bd7c3d11b3fa74ca12bd8070e6ec12f23e2bb9fd
YERIM <dnaer456@gmail.com>
2021-01-23 02:17:12 +0900

    MM12 yet

--- src/routers/MM12/MM12Presenter.jsx
+++ src/routers/MM12/MM12Presenter.jsx
@@ -1,8 +1,20 @@
 import React from "react";
 import styled from "styled-components";
+import Fade from "react-reveal/Fade";
 
+const MM12Wrapper = styled.div`
+  width: ${(props) => props.width || `100%`};
+  height: ${(props) => props.height || `100%`};
+  margin: ${(props) => props.margin || ``};
+  padding: ${(props) => props.padding || ``};
+
+  background-color: ${(props) => props.theme.checkColor};
+
+  display: flex;
+  flex-direction: row;
+`;
 const MM12Presenter = () => {
-  return;
+  return <MM12Wrapper>s</MM12Wrapper>;
 };
 
 export default MM12Presenter;
