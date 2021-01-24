8ed8871f14a752cd2e5ff387e69bfe095f63a640
YERIM <dnaer456@gmail.com>
2021-01-25 00:28:14 +0900

    MM13 img complete

--- src/routers/MM13/MM13Presenter.jsx
+++ src/routers/MM13/MM13Presenter.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import Fade from "react-reveal/Fade";
+import schoolsportsclub from "../../img/schoolsportsclub.png";
+import schoolsportscluba from "../../img/schoolsportscluba.png";
 
 const MM13Wrapper = styled.div`
   width: ${(props) => props.width || `100%`};
@@ -34,12 +36,32 @@ const Title = styled.span`
   text-shadow: 2px 8px 4px #60a3bc, 0px 3px 10px #0a3d62;
 `;
 
+const ImgBox = styled.img`
+  width: 620px;
+  height: 350px;
+
+  margin: 40px 0 40px 60px;
+
+  border-radius: 10px;
+  cursor: pointer;
+
+  &:hover {
+    box-shadow: ${(props) => props.theme.boxShadow};
+    transition: 0.6s;
+  }
+`;
+
 const MM13Presenter = () => {
   return (
     <MM13Wrapper>
       <TitleWrapper>
-        <Title>학교스포츠클럽</Title>
+        <Fade>
+          <Title>⚽️ 학교스포츠클럽이란?</Title>
+        </Fade>
       </TitleWrapper>
+
+      <ImgBox src={schoolsportsclub} />
+      <ImgBox src={schoolsportscluba} />
     </MM13Wrapper>
   );
 };
